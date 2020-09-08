5d093f3d0d447f323af79f1dd4b5551d7fc54c9a
healor <healor@gmail.com>
2020-09-08 14:12:40 +0900

    Update do_smartctl.sh
    Modify smartctl option "-a" => "-x", Check temperature history ( polling time : 10 min)

--- do_smartctl.sh
+++ do_smartctl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-while true; do smartctl -a $1 >> smart_$2.log; sleep 5; done
+while true; do smartctl -x $1 >> smart_$2.log; sleep 5; done
 #while true; do nvme smart-log $1 >> smart_$2.log; sleep 5; done
